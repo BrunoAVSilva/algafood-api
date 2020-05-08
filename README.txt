ef4db70b31dd493037b8f9a427c03b8099dee077
Bruno Silva <brunoalb.silva@hotmail.com>
2020-05-08 13:23:01 -0300

    4.25. Modelando e implementando a atualização de recursos com PUT

--- README.txt
+++ README.txt
@@ -1,2 +1,4 @@
-exec in docker: 
-	docker-compose -f stack.yml up
+exec in docker: docker-compose -f stack.yml up
+	
+Aula Atual
+4.25. Modelando e implementando a atualização de recursos com PUT
--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
@@ -24,7 +23,7 @@ public class Cozinha {
 	private Long id;
 
 	//@JsonIgnore
-	@JsonProperty("titulo")
+	//@JsonProperty("titulo")
 	@Column(name = "nome", nullable = false)
 	private String nome;
 
