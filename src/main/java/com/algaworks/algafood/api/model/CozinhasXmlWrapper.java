6dad95935523dde94439a76c1560dd438800ba40
BrunoAVSilva <brunoalb.silva@hotmail.com>
2020-05-13 13:31:35 -0300

    4.29. Desafio: modelando e implementando a consulta de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/api/model/CozinhasXmlWrapper.java
+++ src/main/java/com/algaworks/algafood/api/model/CozinhasXmlWrapper.java
@@ -1,21 +1,13 @@
 package com.algaworks.algafood.api.model;
 
-import java.util.List;
-
-import com.algaworks.algafood.domain.model.Cozinha;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-
 import lombok.Data;
-import lombok.NonNull;
 
-@JacksonXmlRootElement(localName = "cozinhas")
+//@JacksonXmlRootElement(localName = "cozinhas")
 @Data
 public class CozinhasXmlWrapper {
 
-	@JsonProperty("cozinha")
+	/*@JsonProperty("cozinha")
 	@JacksonXmlElementWrapper(useWrapping = false)
 	@NonNull
-	private List<Cozinha> cozinhas;
+	private List<Cozinha> cozinhas;*/
 }
